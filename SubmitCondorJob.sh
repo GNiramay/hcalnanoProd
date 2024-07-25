717017ae79682b7f39ce24c53f1f75e595d83d08
GNiramay <niramaygogate@gmail.com>
2024-07-25 08:27:04 +0200

    removing global tag, adding correct config file path

--- SubmitCondorJob.sh
+++ SubmitCondorJob.sh
@@ -6,7 +6,11 @@
 # Define variables
 FPATH=$1
 OutPath=$2
-GT=$3
+CMSSWVer=$(ls -1 | grep CMSSW)
+
+# Locate the config file for making hcalnano
+CfgFile=${CMSSWVer}/src/1060.1_RunZeroBias2022D/step2_RAW2DIGI_RECO_USER.py
+
 FNAME=$(basename $FPATH)
 FKEY=`echo "$FNAME" | cut -d'.' -f1`
 echo $FKEY
@@ -17,7 +21,7 @@ echo "universe = workday" > $CondFile
 echo "Executable = BashFile.sh" >> $CondFile
 echo "should_transfer_files = YES" >> $CondFile
 echo "WhenToTransferOutput = ON_EXIT" >>$CondFile
-echo "Transfer_Input_Files = step2_RAW2DIGI_RECO_USER.py" >> $CondFile
+echo "Transfer_Input_Files = ${CfgFile}" >> $CondFile
 printf "Output = StdOut/${FKEY}_%s(Cluster)_%s(ProcId).stdout\n" $ $>> $CondFile
 printf "Error = StdErr/${FKEY}_%s(Cluster)_%s(ProcId).stderr\n" $ $>> $CondFile
 printf "Log = Log/${FKEY}_%s(Cluster)_%s(ProcId).log\n" $ $>> $CondFile
