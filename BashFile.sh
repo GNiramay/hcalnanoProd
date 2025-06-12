ff17135d8c6bc6414908523c251bf3950f944a85
GNiramay <niramaygogate@gmail.com>
2025-06-12 13:39:17 +0200

    Including calibration gap commands, modifying global run command

--- BashFile.sh
+++ BashFile.sh
@@ -17,13 +17,22 @@ cd -
 date
 ls -alht
 
-# Make NanoAOD
+# Make NanoAOD (Global runs)
 time cmsDriver.py step2\
      -s RAW2DIGI,RECO,USER:DPGAnalysis/HcalNanoAOD/hcalNano_cff.hcalNanoTask\
      --conditions auto:run3_data_prompt -n -1\
-     --era Run3 --geometry DB:Extended\
+     --era Run3 --geometry DB:Extended --processName USER\
      --datatier NANOAOD --eventcontent NANOAOD\
      --filein $InRoot --fileout file:$OutRoot
+
+# # Make NanoAOD (Calibration gap)
+# time cmsDriver.py step2\
+#      -s RAW2DIGI,RECO,USER:DPGAnalysis/HcalNanoAOD/hcalNano_cff.hcalNanoTask\
+#      --conditions auto:run3_data_prompt -n -1\
+#      --era Run3 --geometry DB:Extended --processName USER\
+#      --datatier NANOAOD --eventcontent NANOAOD\
+#      --customise DPGAnalysis/HcalNanoAOD/hcalNano_cff.customiseHcalCalib\
+#      --filein $InRoot  --fileout file:$OutRoot
 
 date
 ls -alht
